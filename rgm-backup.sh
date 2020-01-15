730417e4f081ea254638ed5b64de7be8d3fc3806
Vincent FRICOU <vincent@fricouv.eu>
2020-01-15 10:40:38 +0100

    updt: add temporary folder to perform installation actions

--- rgm-backup.sh
+++ rgm-backup.sh
@@ -5,3 +5,4 @@ BkpRetention='7'
 BkpBinary='/usr/local/bin/restic'
 ResticVersion='0.9.6'
 ResticDlURL="https://github.com/restic/restic/releases/download/v${ResticVersion}/restic_${ResticVersion}_linux_amd64.bz2"
+TempWorkDir="/tmp/restic/"
