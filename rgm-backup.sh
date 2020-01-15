f8858ab76cdc5c90a5d659b52384c0c7fbe618e7
Vincent FRICOU <vincent@fricouv.eu>
2020-01-15 09:53:43 +0100

    add: script file with variables definitions

---
+++ rgm-backup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -e
+
+BkpDirectory='/srv/rgm/backup/restic'
+BkpRetention='7'
+BkpBinary='/usr/local/bin/restic'
+ResticVersion='0.9.6'
+ResticDlURL="https://github.com/restic/restic/releases/download/v${ResticVersion}/restic_${ResticVersion}_linux_amd64.bz2"
